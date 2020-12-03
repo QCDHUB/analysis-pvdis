8a62b1b539843dcb84af9dc94d5271f7c0567726
Christopher Cocuzza <ccocuzza@ifarm1901.jlab.org>
2020-12-03 15:33:17 -0500

    update

--- plotgen/pstrange.py
+++ plotgen/pstrange.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import os,sys
 import kmeanconf as kc
+import matplotlib
+matplotlib.use('Agg')
+import pylab as py
 
 #--from corelib
 from analysis.corelib import core, inspect, predict, classifier, optpriors, jar, mlsamples, summary
@@ -8,14 +11,8 @@ from analysis.corelib import core, inspect, predict, classifier, optpriors, jar,
 #--from qpdlib
 from analysis.qpdlib import ppdf
 
-#--primary working directory
-wdir=sys.argv[1]
-
-#--look for extra working directories
-try: wdir2 = sys.argv[2]
-except: wdir2 = None
-try: wdir3 = sys.argv[3]
-except: wdir3 = None
+wdir  = 'results/pidis0'
+wdir2 = 'results/pidis1'
 
 
 ####################
@@ -25,55 +22,49 @@ except: wdir3 = None
 #--plot multiple steps on the same plot (wdir, Q2, color, style, label, alpha)
 Q21    = 10
 Q22    = 10
-Q23    = 10
 
 color1 = 'red'
 color2 = 'red'
-color3 = 'green'
 
 label1 = None 
 label2 = None 
-label3 = None
  
-label1 = r'\textbf{\textrm{Optimistic}}'
-label2 = r'\textbf{\textrm{Moderate}}'
-label3 = r'\textbf{\textrm{No EIC}}'
 
 ls1    = '-'
 ls2    = '-'
-ls3    = '-'
 
 alpha1 = 0.5
 alpha2 = 0.2
-alpha3 = 0.2
 
 PLOT = []
 PLOT.append((wdir,Q21,color1,ls1,label1,alpha1))
 
-if wdir2 != None: PLOT.append((wdir2,Q22,color2,ls2,label2,alpha2))
-if wdir3 != None: PLOT.append((wdir3,Q23,color3,ls3,label3,alpha3))
+PLOT.append((wdir2,Q22,color2,ls2,label2,alpha2))
 
-Q2 = Q21
-
-#--name to append at end of file
-name = '-compare'
-#name = '-LHC-compare'
-
-#--maximum number of replicas to plot
-nrep = None
-#nrep = 50
-
-#--mode: 0 for all replicas, 1 for mean and std
-mode = 0
 
 ########################
 #--Polarized proton pdfs
 ########################
 PSETS = []
 
-ppdf.plot_xf(PLOT,kc,kind=1)
-ppdf.plot_moments(PLOT,kc,mode=1)
+nrows,ncols=1,2
+fig = py.figure(figsize=(ncols*8,nrows*5))
+_ax11 = py.subplot(nrows,ncols,1) 
+_ax12 = py.subplot(nrows,ncols,2) 
 
+ax11 = ppdf.plot_xf               (PLOT,kc,kind=1,_ax=_ax11)    
+ax12 = ppdf.plot_moments_std_ratio(PLOT,kc,mode=1,_ax=_ax12)    
+
+ax12.tick_params(labelleft=False)
+ax12.set_xticks([1e-3,1e-2,1e-1])
+
+py.tight_layout()
+py.subplots_adjust(wspace=0)
+
+filename = '%s/gallery/ppdfs-combined'%PLOT[0][0]
+filename += '.png'
+print('Saving figures to %s'%filename)
+py.savefig(filename)
 
 
 
